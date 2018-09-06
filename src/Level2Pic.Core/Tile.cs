59a92726dc420e2a2291d492f5e2a2c815ea088e
Richard Lim <rclim95@outlook.com>
2018-09-05 23:05:22 -0500

    Add a Tile class
    The Tile class stores information about a tile that can be placed in a
    level. In addition to some metadata information useful to the user
    (i.e., its ID, the name, and an optional description), the Tile class
    also contains a reference to the Texture that should be used whenever
    a visual representation of the Tile is needed.

---
+++ src/Level2Pic.Core/Tile.cs
@@ -0,0 +1,36 @@
+﻿// <copyright file="Tile.cs" company="rclim95 Productions">
+// Copyright © rclim95 Productions
+//
+// This project is licensed under the MIT License. For more information, see LICENSE.md.
+// </copyright>
+
+namespace Level2Pic.Core
+{
+    using System;
+
+    /// <summary>
+    /// Represents a tile that can be placed in a level.
+    /// </summary>
+    public class Tile
+    {
+        /// <summary>
+        /// Gets or sets a description of the tile.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ID of the tile.
+        /// </summary>
+        public int ID { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the tile.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the texture to use to show this tile.
+        /// </summary>
+        public Texture Texture { get; set; }
+    }
+}
